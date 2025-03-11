52f002460b2b3aa7b2d31d4516300b5a2d252760
modalsoul0226 <modalsoul0226@gmail.com>
2025-03-10 21:00:41 -0400

    fix: update css resolve path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+/**
+ * Relative paths and aliases in Nuxt layers
+ * see: https://nuxt.com/docs/guide/going-further/layers#relative-paths-and-aliases
+ */
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   devServer: {
-      port: 3010
+    port: 3010
   },
   modules: ["@unocss/nuxt", "@nuxt/fonts"],
-  css: ["~/assets/css/main.css", "~/assets/css/reset.css", "@unocss/reset/normalize.css"],
+  css: [
+    join(currentDir, "./assets/css/main.css"),
+    join(currentDir, "./assets/css/reset.css"),
+    "@unocss/reset/normalize.css"
+  ],
 });
